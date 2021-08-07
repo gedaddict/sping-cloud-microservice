7a60c28febfe16161d3429b62b54f38c77a5b443
Ged Tajonera <ged.mystyle@gmail.com>
2021-08-07 16:24:49 +0800

    add config server

---
+++ config-weather-catalogue/src/test/java/com/example/configweathercatalogue/ConfigWeatherCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configweathercatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigWeatherCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
